5db131428f3b4f56490e8c958d2f1834f6934d10
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-26 20:41:12 +0100

    setup: Initial configuration of Node.js and Express

---
+++ back-end/src/api/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req, res) => res.status(200).json({ message: 'Hello Back-end!' }))
+
+module.exports = app;
