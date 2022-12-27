0437b02cefb635d0e12b566c919345cb5431c610
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-27 12:35:08 +0100

    feat: Add migration for cnpjs table

---
+++ back-end/src/database/migrations/20221227112718-cnpjs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cnpjs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      cnpj: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      company_type: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('cnpjs');
+  }
+};
