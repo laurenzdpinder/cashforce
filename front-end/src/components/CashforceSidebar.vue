76442001e3d05c8d69ab9c584776b5342cb4967a
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-29 00:53:30 +0100

    feat: Add CashforceSidebar component

---
+++ front-end/src/components/CashforceSidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside class="cash-force">
+    <h1>Cashforce</h1>
+    <p>Notas fiscais</p>
+  </aside>
+</template>
+
+<script>
+
+export default {
+  name: 'CashforceSidebar',
+};
+
+</script>
+
+<style>
+aside {
+  width: 288px;
+}
+</style>
