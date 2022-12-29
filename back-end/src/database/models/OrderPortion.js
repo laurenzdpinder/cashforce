ef401056d9826410ba3ce3972ce90b2a982e51fa
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-29 15:51:46 +0100

    feat: Add OrderPortion model

---
+++ back-end/src/database/models/OrderPortion.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('Sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  n_dup: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  d_venc: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  v_dup: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  available_to_market: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  order_id: {
+    type: DataTypes.INTEGER,
+  }
+};
+
+module.exports = (sequelize) => {
+  const OrderPortion = sequelize.define(
+    'OrderPortion',
+    Attributes,
+    {
+      tableName: 'orderportions',
+      underscore: true,
+      timestamps: true,
+    }
+  );
+
+  return OrderPortion;
+};
