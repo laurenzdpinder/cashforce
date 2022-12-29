624d19b6027a9c8ce3db423c334cb24c9148c49c
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-29 15:50:49 +0100

    feat: Add Buyer model

---
+++ back-end/src/database/models/Buyer.js
@@ -0,0 +1,100 @@
+const { DataTypes } = require('Sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tradingName: {
+    type: DataTypes.STRING
+  },
+  cashforceTax: {
+    type: DataTypes.STRING
+  },
+  responsibleName: {
+    type: DataTypes.STRING
+  },
+  responsibleEmail: {
+    type: DataTypes.STRING
+  },
+  responsiblePosition: {
+    type: DataTypes.STRING
+  },
+  responsiblePhone: {
+    type: DataTypes.STRING
+  },
+  responsibleMobile: {
+    type: DataTypes.STRING
+  },
+  website: {
+    type: DataTypes.STRING
+  },
+  postalCode: {
+    type: DataTypes.STRING
+  },
+  address: {
+    type: DataTypes.STRING
+  },
+  number: {
+    type: DataTypes.STRING
+  },
+  complement: {
+    type: DataTypes.STRING
+  },
+  neighborhood: {
+    type: DataTypes.STRING
+  },
+  city: {
+    type: DataTypes.STRING
+  },
+  state: {
+    type: DataTypes.STRING
+  },
+  phoneNumber: {
+    type: DataTypes.STRING
+  },
+  situation: {
+    type: DataTypes.STRING
+  },
+  situationDate: {
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  cnpjId: {
+    type: DataTypes.INTEGER
+  },
+  confirm: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  email: {
+    type: DataTypes.STRING
+  }
+};
+
+module.exports = (sequelize) => {
+  const Buyer = sequelize.define(
+    'Buyer',
+    Attributes,
+    {
+      tableName: 'buyers',
+      underscore: true,
+      timestamps: true,
+    }
+  );
+
+  return Buyer;
+};
