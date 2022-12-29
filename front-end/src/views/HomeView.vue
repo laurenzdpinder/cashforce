1d02efa0d36ed265235dda616203d29fe74f501a
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-29 01:04:23 +0100

    feat: remove default code and add new basic structure for Homeview component

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <CashforceSidebar />
+    </div>
+    <div>
+      <h1>Home</h1>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
+import CashforceSidebar from '@/components/CashforceSidebar.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    CashforceSidebar,
   },
 };
+
 </script>
+
+<style scoped>
+.home {
+  border: 1px solid red;
+  display: flex;
+}
+
+.home div {
+  border: 1px solid black;
+}
+
+.home h1 {
+  border: 1px solid black;
+}
+</style>
