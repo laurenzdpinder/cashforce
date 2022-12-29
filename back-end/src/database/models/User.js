d9ec5598663784c34b52c967d9dad93091768331
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-29 15:50:16 +0100

    feat: Add User model

---
+++ back-end/src/database/models/User.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require('Sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phoneNumber: {
+    type: DataTypes.STRING
+  },
+  mobile: {
+    type: DataTypes.STRING
+  },
+  departament: {
+    type: DataTypes.STRING
+  },
+  verificationCode: {
+    type: DataTypes.STRING
+  },
+  emailChecked: {
+    type: DataTypes.BOOLEAN
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  cashforceAdm: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+};
+
+module.exports = (sequelize) => {
+  const User = sequelize.define(
+    'User',
+    Attributes,
+    {
+      tableName: 'users',
+      underscore: true,
+      timestamps: true,
+    }
+  );
+
+  return User;
+};
