812e496559adcfd3150346615a06ab0b7e699170
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-30 17:13:06 +0100

    feat: Add CashforceTable component

---
+++ front-end/src/components/CashforceTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Nota Fiscal</th>
+        <th>Sacado</th>
+        <th>Cedente</th>
+        <th>Emissão</th>
+        <th>Valor</th>
+        <th>Status</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="order in orders" :key="order.id">
+        <td>{{ order.nNf }}</td>
+        <td>{{ order.buyer.name }}</td>
+        <td>{{ order.provider.name }}</td>
+        <td>{{ order.emissionDate }}</td>
+        <td>{{ order.value }}</td>
+        <td>{{ order.orderStatusBuyer }}</td>
+        <button>Dados do cedente</button>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import fetchOrders from '../api/ordersApi';
+
+export default {
+  name: 'CashforceTable',
+  data() {
+    return {
+      orders: [],
+    };
+  },
+  async mounted() {
+    const orders = await fetchOrders();
+    this.orders = orders;
+    console.log(orders);
+  },
+};
+</script>
+
+<style>
+th {
+  border: 1px solid black;
+  width: 140px;
+}
+</style>
--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
-    <div>
+    <div class="test1">
       <CashforceSidebar />
     </div>
-    <div>
+    <div class="test2">
       <h1>Home</h1>
+      <CashforceTable />
     </div>
   </div>
 </template>
@@ -12,11 +13,13 @@
 <script>
 
 import CashforceSidebar from '@/components/CashforceSidebar.vue';
+import CashforceTable from '@/components/CashforceTable.vue';
 
 export default {
   name: 'HomeView',
   components: {
     CashforceSidebar,
+    CashforceTable,
   },
 };
 
@@ -24,12 +27,18 @@ export default {
 
 <style scoped>
 .home {
-  border: 1px solid red;
+  border: 1px solid black;
   display: flex;
 }
 
-.home div {
-  border: 1px solid black;
+.test1 {
+  border: 2px solid blue;
+  width: 20%;
+}
+
+.test2 {
+  border: 2px solid red;
+  width: 80%;
 }
 
 .home h1 {
