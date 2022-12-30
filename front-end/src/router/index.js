b2a01c04aebd047a1560c701cd8de38281fab8e9
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-30 18:12:25 +0100

    feat: Add ProviderView page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import ProviderView from '../views/ProviderView.vue';
 
 const routes = [
   {
@@ -7,14 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView,
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  // },
+  {
+    path: '/providers/:id',
+    name: 'providers',
+    component: ProviderView,
+  },
 ];
 
 const router = createRouter({
