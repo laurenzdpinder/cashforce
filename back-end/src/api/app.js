f28349290b245a08d425184c79845bb6787aa59a
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-30 12:58:42 +0100

    feat: Add error middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const error = require('../middlewares/error');
 const routes = require('../routes');
 
 const app = express();
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use('/orders', routes.orderRouter);
 app.use('/providers', routes.providerRouter);
 app.use('/buyers', routes.buyerRouter);
+
+app.use(error);
 
 module.exports = app;
