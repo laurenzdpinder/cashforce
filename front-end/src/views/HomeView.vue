cc68758f03570474e2f2806058a612ec9c2357ce
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-12-31 02:56:39 +0100

    style: Start CSS for HomeView page

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
     <CashforceSidebar />
-    <div class="test2">
-      <h1>Home</h1>
+    <div class="home-main">
+      <div class="blank-space" />
+      <div class="home-title">
+        <h3>Notas fiscais</h3>
+        <p>Vizualize as notas fiscais que você tem.</p>
+      </div>
       <CashforceTable />
     </div>
   </div>
@@ -23,15 +27,45 @@ export default {
 
 <style>
 .home {
-  border: 1px solid black;
+  border: 2px solid black;
   display: flex;
+  font-family: DM Sans;
 }
 
-.test2 {
-  border: 2px solid red;
+.home-main {
+  border: 1px solid red;
+  margin: 0 48px;
+  width: 100%;
 }
 
-.home h1 {
+.blank-space {
+  border-bottom: 1px solid #DFE2EB;
+  height: 64px;
+  margin-bottom: 40px;
+}
+
+.home-title {
   border: 1px solid black;
+  text-align: left;
+}
+
+.home-title h3 {
+  border: 1px solid black;
+  color: #021B51;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 28px;
+  margin: 0;
+}
+
+.home-title p {
+  border: 1px solid black;
+  color: #727D94;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 0;
 }
 </style>
