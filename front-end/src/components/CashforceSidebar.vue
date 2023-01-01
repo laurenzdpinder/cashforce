f5576b85abcdc17baac69c4722a36f9c35bbec15
Laurenz Pinder <laurenzdpinder90@gmail.com>
2023-01-01 19:19:02 +0100

    style: Prepare CashforceSidebar for logo

--- front-end/src/components/CashforceSidebar.vue
+++ front-end/src/components/CashforceSidebar.vue
@@ -1,7 +1,12 @@
 <template>
-  <aside class="cash-force">
-    <h1>Cashforce</h1>
-    <p>Notas fiscais</p>
+  <aside>
+    <div>
+      <h1>Cashforce</h1>
+    </div>
+    <div>
+      <p>img</p>
+      <p>Notas fiscais</p>
+    </div>
   </aside>
 </template>
 
@@ -16,7 +21,38 @@ export default {
 <style>
 aside {
   background-color: #FBFDFE;
-  /* border: 1px solid black; */
   min-width: 288px;
+}
+
+aside div:nth-child(1) {
+  /* border: 1px solid black; */
+  height: 80px;
+}
+
+aside div:nth-child(2) {
+  /* border: 1px solid black; */
+  display: flex;
+  margin-top: 23px;
+}
+
+aside p:nth-child(1) {
+  color: #00AD8C;
+  line-height: 18px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  text-align: left;
+  padding-left: 25px;
+}
+
+aside p:nth-child(2) {
+  /* border: 1px solid black; */
+  color: #00AD8C;
+  line-height: 18px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  text-align: left;
+  padding: 0 15px;
 }
 </style>
