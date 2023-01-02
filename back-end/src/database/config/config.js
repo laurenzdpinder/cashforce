ab8377c4dc3b70018e46b4a418e0630a5643980c
Laurenz Pinder <laurenzdpinder90@gmail.com>
2023-01-02 17:24:17 +0100

    setup: Update .env vars

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,27 +1,15 @@
 require('dotenv').config();
 
-// const config = {
-//   username: process.env.MYSQL_USER || 'root',
-//   password: process.env.MYSQL_PASSWORD || 'password',
-//   database: 'cashforce',
-//   host: process.env.MYSQL_HOST || 'localhost',
-//   port: process.env.MYSQL_PORT || 3306,
-//   dialect: 'mysql'
-// }
-
-// module.exports = {
-//   development: config,
-//   test: config,
-//   production: config,
-// };
-
+const config = {
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: 'cashforce',
+  host: process.env.MYSQL_HOST || 'localhost',
+  dialect: 'mysql'
+}
 
 module.exports = {
-  development: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
-    database: 'cashforce',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql'
-  }
-}
+  development: config,
+  test: config,
+  production: config,
+};
