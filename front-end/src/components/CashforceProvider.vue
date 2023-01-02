5eeb10cea4d0519094579e86cc2633b757e0711e
Laurenz Pinder <laurenzdpinder90@gmail.com>
2023-01-02 14:35:18 +0100

    style: Start CSS for ProviderView and CashforceProvider

--- front-end/src/components/CashforceProvider.vue
+++ front-end/src/components/CashforceProvider.vue
@@ -1,18 +1,32 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <td>Nome</td>
-        <td>Nome Comercial</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ provider.name }}</td>
-        <td>{{ provider.tradingName }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="provider-info">
+    <div>
+      <p>Nome</p>
+      <p>Nome Comercial</p>
+      <p>CNPJ</p>
+      <p>Telefone</p>
+      <p>E-mail</p>
+      <p>Website</p>
+      <p>Endereço</p>
+      <p>Cidade</p>
+      <p>Estado</p>
+      <p>Banco</p>
+      <p>Agência e Conta</p>
+    </div>
+    <div>
+      <p>{{ provider.name }}</p>
+      <p>{{ provider.tradingName }}</p>
+      <p>Mock</p>
+      <p>Mock</p>
+      <p>{{ provider.email || 'Não informado' }}</p>
+      <p>Mock</p>
+      <p>Mock</p>
+      <p>Mock</p>
+      <p>Mock</p>
+      <p>Mock</p>
+      <p>Mock</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -32,3 +46,38 @@ export default {
   },
 };
 </script>
+
+<style>
+.provider-info {
+  /* border: 1px solid black; */
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  padding: 10px 0;
+  width: 100%;
+}
+
+/* .provider-info div:nth {
+  border: 1px solid black;
+  margin: 10px;
+} */
+
+.provider-info p {
+  border-bottom: 1px solid #A1A8B8;
+  color: #A1A8B8;
+  line-height: 16px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  padding: 15px 275px 5px 25px;
+  text-align: left;
+}
+
+/* .provider-info div:nth-child(1) {
+  border: 1px solid red;
+}
+
+.provider-info div:nth-child(2) {
+  border: 1px solid blue;
+} */
+</style>
--- front-end/src/views/ProviderView.vue
+++ front-end/src/views/ProviderView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="provider">
     <CashforceSidebar />
-    <div>
-      <h1>Hello, Provider</h1>
-      <CashforceProvider />
+    <div class="provider-main">
+      <div class="blank-space" />
+      <div class="provider-title">
+        <h3>Dados do Cedente</h3>
+        <CashforceProvider />
+      </div>
     </div>
   </div>
 </template>
@@ -21,9 +24,40 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .provider {
-  border: 1px solid black;
   display: flex;
+  min-width: 1340px;
+}
+
+.provider-main {
+  /* align-items: center; */
+  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.provider-title {
+  /* align-items: center; */
+  /* border: 1px solid green; */
+  /* display: flex;
+  flex-direction: column; */
+}
+
+.provider-main h3 {
+  /* border-bottom: 1px solid #021B51; */
+  /* border: 1px solid #021B51; */
+  /* display: flex; */
+  color: #021B51;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 28px;
+  /* margin: 10px; */
+  /* margin-left: 135px; */
+  margin-bottom: 10px;
+  text-align: center;
+  /* width: 23%; */
 }
 </style>
