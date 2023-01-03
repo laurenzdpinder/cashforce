b341d7fe2537678c2a1f97ffa3d0a2faeaabde93
Laurenz Pinder <laurenzdpinder90@gmail.com>
2023-01-03 02:10:41 +0100

    style: Complete CSS for ProviderView page and CashforceProvider component

--- front-end/src/components/CashforceProvider.vue
+++ front-end/src/components/CashforceProvider.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="provider-info">
     <div>
+      <h3>Dados do cedente</h3>
       <p>Nome</p>
       <p>Nome Comercial</p>
       <p>CNPJ</p>
@@ -14,17 +15,18 @@
       <p>Agência e Conta</p>
     </div>
     <div>
+      <h3 style="color: #FFFFFF;">''</h3>
       <p>{{ provider.name }}</p>
       <p>{{ provider.tradingName }}</p>
-      <p>Mock</p>
-      <p>Mock</p>
+      <p>{{ provider.cnpj }}</p>
+      <p>{{ provider.number || 'Não informado' }}</p>
       <p>{{ provider.email || 'Não informado' }}</p>
-      <p>Mock</p>
-      <p>Mock</p>
-      <p>Mock</p>
-      <p>Mock</p>
-      <p>Mock</p>
-      <p>Mock</p>
+      <p>{{ provider.website || 'Não informado' }}</p>
+      <p>{{ provider.adress || 'Não informado' }}</p>
+      <p>{{ provider.city || 'Não informado' }}</p>
+      <p>{{ provider.state || 'Não informado' }}</p>
+      <p>{{ provider.bank || 'Não informado' }}</p>
+      <p>{{ provider.bankAgency || 'Não informado' }}</p>
     </div>
   </div>
 </template>
@@ -41,8 +43,7 @@ export default {
   },
   async mounted() {
     const provider = await fetchProviderbyId(this.$route.params.id);
-    this.provider = provider;
-    console.log(provider);
+    this.provider = { ...provider, cnpj: provider.cnpj.cnpj };
   },
 };
 </script>
@@ -53,14 +54,23 @@ export default {
   display: flex;
   justify-content: center;
   margin-top: 50px;
-  padding: 10px 0;
+  /* padding: 10px 0; */
   width: 100%;
 }
 
-/* .provider-info div:nth {
-  border: 1px solid black;
-  margin: 10px;
-} */
+.provider-info h3 {
+  border-bottom: 1px solid #021B51;
+  /* border: 1px solid #021B51; */
+  color: #021B51;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 28px;
+  /* padding: 15px 275px 5px 25px; */
+  /* padding-left: 25px; */
+  margin-bottom: 30px;
+  text-align: left;
+}
 
 .provider-info p {
   border-bottom: 1px solid #A1A8B8;
@@ -69,15 +79,17 @@ export default {
   font-size: 16px;
   font-style: normal;
   font-weight: 700;
-  padding: 15px 275px 5px 25px;
+  padding: 15px 300px 5px 25px;
   text-align: left;
 }
 
-/* .provider-info div:nth-child(1) {
-  border: 1px solid red;
+.provider-info div:nth-child(1) {
+  /* border: 1px solid red; */
+  /* width: 100%; */
 }
 
 .provider-info div:nth-child(2) {
-  border: 1px solid blue;
-} */
+  /* border: 1px solid blue; */
+  /* width: 100%; */
+}
 </style>
--- front-end/src/views/ProviderView.vue
+++ front-end/src/views/ProviderView.vue
@@ -3,10 +3,7 @@
     <CashforceSidebar />
     <div class="provider-main">
       <div class="blank-space" />
-      <div class="provider-title">
-        <h3>Dados do Cedente</h3>
-        <CashforceProvider />
-      </div>
+      <CashforceProvider />
     </div>
   </div>
 </template>
@@ -31,33 +28,6 @@ export default {
 }
 
 .provider-main {
-  /* align-items: center; */
-  /* border: 1px solid red; */
-  display: flex;
-  flex-direction: column;
   width: 100%;
-}
-
-.provider-title {
-  /* align-items: center; */
-  /* border: 1px solid green; */
-  /* display: flex;
-  flex-direction: column; */
-}
-
-.provider-main h3 {
-  /* border-bottom: 1px solid #021B51; */
-  /* border: 1px solid #021B51; */
-  /* display: flex; */
-  color: #021B51;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 28px;
-  /* margin: 10px; */
-  /* margin-left: 135px; */
-  margin-bottom: 10px;
-  text-align: center;
-  /* width: 23%; */
 }
 </style>
