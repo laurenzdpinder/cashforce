4de61ef4411c50ffa9aa90bd649a60ef4b727906
Laurenz Pinder <laurenzdpinder90@gmail.com>
2023-07-07 15:01:34 +0200

    chore: Update config.js - host

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -4,7 +4,7 @@ const config = {
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   database: 'cashforce',
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOST || '127.0.0.1',
   dialect: 'mysql'
 }
 
